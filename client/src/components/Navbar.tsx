0d9fa3eab2537b692c960b5c412c715075f15518
JoaoHaubert <jhb.haubert@gmail.com>
2023-06-29 17:33:30 -0300

    Navbar Left/Right components added
    -Added Menu button for sidebar to open
    -Added Searchbox and icons for it
    -Added button add and notifications

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,20 +1,20 @@
 import React, { useState } from "react";
-import {
-  Menu as MenuIcon,
-  Search,
-  SettingsOutlined,
-  ArrowDropDownOutlined,
-} from "@mui/icons-material";
-import FlexBetween from "./FlexBetween";
 import { useDispatch } from "react-redux";
+//MUI IMPORTS
+import { Menu as MenuIcon, Search } from "@mui/icons-material";
+import { Icon, InputBase, useTheme } from "@mui/material";
+import { AppBar, IconButton, Toolbar } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+//COMPONENTS IMPORTS
+import FlexBetween from "./FlexBetween";
+//SVG IMPORTS
 import profileImage from "@/assets/react.svg";
-import { useTheme } from "@emotion/react";
-import { AppBar, Toolbar } from "@mui/material";
 
 type Props = {};
 
 export default function Navbar({}: Props) {
-  const theme = useTheme();
+  const { palette } = useTheme();
   const dispatch = useDispatch();
 
   return (
@@ -25,9 +25,33 @@ export default function Navbar({}: Props) {
         boxShadow: "none",
       }}
     >
-      <Toolbar sx={{justifyContent: "space-between"}}>
+      <Toolbar sx={{ justifyContent: "space-between" }}>
         {/*>>>LEFT SIDE<<<*/}
-        
+        <FlexBetween>
+          <IconButton onClick={() => console.log("sidebar open")}>
+            <MenuIcon />
+          </IconButton>
+          <FlexBetween
+            bgcolor={palette.background.default}
+            borderRadius="9px"
+            gap="3rem"
+            p="0.1rem 1.5rem"
+          >
+            <InputBase placeholder="Search..." />
+            <IconButton>
+              <Search />
+            </IconButton>
+          </FlexBetween>
+        </FlexBetween>
+        {/*>>>RIGHT SIDE<<<*/}
+        <FlexBetween gap="1.5rem">
+          <IconButton onClick={() => console.log("add button")}>
+            <AddIcon sx={{ fontSize: "25px" }} />
+          </IconButton>
+          <IconButton onClick={() => console.log("notification button")}>
+            <NotificationsIcon sx={{ fontSize: "25px" }} />
+          </IconButton>
+        </FlexBetween>
       </Toolbar>
     </AppBar>
   );
--- client/src/views/layout/index.tsx
+++ client/src/views/layout/index.tsx
@@ -3,15 +3,22 @@ import { Box, useMediaQuery } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
 
 type Props = {};
 
 export default function Layout({}: Props) {
-  return <Box width="100%" height="100%">
-    <Box>
+  const isNonMobile = useMediaQuery("(min-width: 600px)");
+  const [isSidebarOpen, setIsSidebarOpen] = React.useState(true);
+  return (
+    <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
+        <Sidebar
+            isNonMobile={isNonMobile}
+        />
+      <Box>
         <Navbar />
         <Outlet />
+      </Box>
     </Box>
-
-  </Box>
+  );
 }
