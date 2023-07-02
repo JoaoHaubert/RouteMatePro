7ff4d0e4fe14fc681e42cc95b18ce8485ee78be9
JoaoHaubert <jhb.haubert@gmail.com>
2023-07-02 13:20:56 -0300

    Adding Sidebar component
    - Sidebar component added
    - Icons added in Sidebar component

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,7 +1,49 @@
-import React from 'react'
-
+import React, { ReactNode } from 'react'
+//icons
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import AppRegistrationIcon from '@mui/icons-material/AppRegistration';
+import DirectionsCarIcon from '@mui/icons-material/DirectionsCar';
+import NoCrashIcon from '@mui/icons-material/NoCrash';
+import BuildIcon from '@mui/icons-material/Build';
+import PeopleIcon from '@mui/icons-material/People';
+import StoreIcon from '@mui/icons-material/Store';
+import HistoryIcon from '@mui/icons-material/History';
 type Props = {}
 
+const navItems = [
+  {
+    text: "Dashboard",
+    icon: <DashboardIcon />,
+  },
+  {
+    text: "Cadastros",
+    icon: <AppRegistrationIcon />
+  },
+  {
+    text: "Veiculos",
+    icon: <DirectionsCarIcon />,
+  },
+  {
+    text: "Veiculos Ativos",
+    icon: <NoCrashIcon />,
+  },
+  {
+    text: "Manutencoes",
+    icon: <BuildIcon />,
+  },
+  {
+    text: "Condutores",
+    icon: <PeopleIcon />,
+  },
+  {
+    text: "Lojas",
+    icon: <StoreIcon />,
+  },
+  {
+    text: "Historico",
+    icon: <HistoryIcon />,
+  },
+]
 export default function Sidebar({}: Props) {
   return (
     <div>Sidebar</div>
--- client/src/views/layout/index.tsx
+++ client/src/views/layout/index.tsx
@@ -1,22 +1,32 @@
-import React from "react";
+//@ts-nocheck
+import { ReactNode, useState, HTMLAttributes } from "react";
 import { Box, useMediaQuery } from "@mui/material";
 import { Outlet } from "react-router-dom";
-import { useSelector } from "react-redux";
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
 
-type Props = {};
+
+
+
+type Props = {}
+
 
 export default function Layout({}: Props) {
-  const isNonMobile = useMediaQuery("(min-width: 600px)");
-  const [isSidebarOpen, setIsSidebarOpen] = React.useState(true);
+  const isNonMobile = useMediaQuery<string>("(min-width: 600px)");
+  const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(true);
   return (
-    <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
+    <Box  display={isNonMobile ? 'flex' : 'block'} width="100%" height="100%">
         <Sidebar
-            isNonMobile={isNonMobile}
+          isNonMobile={isNonMobile}
+          drawerWidth="250px"
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
+        /> 
+      <Box flexGrow={1}>
+        <Navbar
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
         />
-      <Box>
-        <Navbar />
         <Outlet />
       </Box>
     </Box>
