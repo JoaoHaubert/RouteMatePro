1c9b52aa9375b13aecb6c4c69bf436d066ee838e
JoaoHaubert <jhb.haubert@gmail.com>
2023-07-10 16:37:07 -0300

    Fixed types on general controller
    - Fixed general - controller
    - Still trying to receive data from mongodb

--- server/controllers/general.ts
+++ server/controllers/general.ts
@@ -1,11 +1,12 @@
+import { Request, Response } from "express";
 import User from "../models/User";
 
-export const getUser = async (req: any, res: any) => {
-    try {
-        const { id } = req.params;
-        const user = await User.findById(id)
-        res.status(200).json(user)
-    } catch(error: any) {
-        res.status(404).json({message: error.message})
-    }
-}
+export const getUser = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    res.status(200).json(user);
+  } catch (error: any) {
+    res.status(404).json({ message: error.message });
+  }
+};
