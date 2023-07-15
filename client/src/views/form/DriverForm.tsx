e75adcae711ab97fecf12046758121861f4471f1
JoaoHaubert <jhb.haubert@gmail.com>
2023-07-14 21:05:36 -0300

    Added folder form in views
    - Added forms for vehicles, drivers and shops.

---
+++ client/src/views/form/DriverForm.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export default function DriverForm({}: Props) {
+  return (
+    <div>DriverForm</div>
+  )
+}
---
+++ client/src/views/form/VehicleForm.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export default function VehicleForm({}: Props) {
+  return (
+    <div>VehicleForm</div>
+  )
+}
