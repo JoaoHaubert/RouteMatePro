9c30f0bca25948ff7cf5706a6b4e25192fa2fcfe
JoaoHaubert <jhb.haubert@gmail.com>
2023-07-20 18:34:01 -0300

    Buttons aligned, next split 3 different forms

--- client/src/components/SaveButton.tsx
+++ client/src/components/SaveButton.tsx
@@ -1,38 +1,46 @@
-import React from 'react'
-import { Box, Button } from '@mui/material' 
+import React from "react";
+import { Box, Button } from "@mui/material";
 
-type Props = {}
+type Props = {};
 
 export default function SaveButton({}: Props) {
   return (
     <Box
-    display="flex"
-    justifyContent="space-between"
-    alignItems="center"
-    padding={1}
-  >
-    {/* Left Side */}
-    <Button  size="small" color="error"
-        sx={{
-            marginLeft: "3rem"
-        }}
+      display="flex"
+      justifyContent="space-between"
+      alignItems="center"
+      padding={1}
     >
-      Cancelar
-    </Button>
+      {/* Left Side */}
+      <Button
+        size="small"
+        color="error"
+        sx={{
+          marginLeft: "3rem",
+        }}
+      >
+        Cancelar
+      </Button>
 
-    {/* Right Side Container */}
-    <Box>
-      <Button size="small" variant="outlined" color="info"
-        sx={{marginRight: "1rem"}}
-      >
-        Salvar e adicionar outro
-      </Button>
-      <Button size="small" variant="contained" color="primary"
-        sx={{marginRight: "3rem"}}
-      >
-        Salvar
-      </Button>
+      {/* Right Side Container */}
+      <Box>
+        <Button
+          size="small"
+          variant="outlined"
+          color="info"
+          sx={{ marginRight: "1rem" }}
+        >
+          Salvar e adicionar outro
+        </Button>
+        <Button
+          size="small"
+          variant="contained"
+          color="primary"
+          sx={{ marginRight: "3rem" }}
+        >
+          Salvar
+        </Button>
+      </Box>
     </Box>
-  </Box>
-  )
+  );
 }
