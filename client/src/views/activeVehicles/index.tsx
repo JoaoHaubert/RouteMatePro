8f556121e39c00fc2c00494aa35236c9a5b0fb3f
JoaoHaubert <jhb.haubert@gmail.com>
2023-10-18 19:19:06 -0300

    Just a few implementations in the activeVehicles

--- client/src/views/activeVehicles/index.tsx
+++ client/src/views/activeVehicles/index.tsx
@@ -1,12 +1,33 @@
 import React from "react";
-import { Box } from "@mui/material"
+import { Box, Typography, Button } from "@mui/material";
 
 const ActiveVehicles: React.FC = () => {
-    return(
-        <Box>
-            Planejamento de veiculos ativos
-        </Box>
-    )
-}
+  return (
+    <Box>
+      <Box
+        bgcolor="white"
+        m="0.3rem 0rem"
+        p="0.8rem"
+        borderRadius="8px"
+        border="solid 2px #DDE6ED"
+        flexDirection="column"
+      >
+        <Typography variant="h2" m="0.2rem 0.5rem">
+          Veículos Ativos
+        </Typography>
+        <Button
+        onClick={() => {console.log("clicked")}}
+          type="submit"
+          size="small"
+          variant="contained"
+          color="primary"
+          sx={{ marginLeft: "0.5rem" }}
+        >
+          Criar Serviço
+        </Button>
+      </Box>
+    </Box>
+  );
+};
 
-export default ActiveVehicles
+export default ActiveVehicles;
