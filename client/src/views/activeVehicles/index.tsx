0afed35208e6c2631e559f5188e1ad34af8fd70d
JoaoHaubert <jhb.haubert@gmail.com>
2023-10-17 21:37:43 -0300

    Starting active vehicles view

---
+++ client/src/views/activeVehicles/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box } from "@mui/material"
+
+const ActiveVehicles: React.FC = () => {
+    return(
+        <Box>
+            Planejamento de veiculos ativos
+        </Box>
+    )
+}
+
+export default ActiveVehicles
